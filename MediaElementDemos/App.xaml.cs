d2662b8293c3a40f595e8880d0dd4e5515111996
Marc <marc@marcbruins.nl>
2020-09-08 10:11:07 +0200

    add capture

---
+++ MediaElementDemos/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace MediaElementDemos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Device.SetFlags(new string[] { "MediaElement_Experimental" });
+            InitializeComponent();
+        }
+
+        protected override async void OnStart()
+        {
+            //  await FileAccess.CopyVideoIfNotExists("XamarinForms101UsingEmbeddedImages.mp4");
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
